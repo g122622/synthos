a02e7a57556f426a550861670b1cf68e94a4d1e7
g122622 <3300925806@qq.com>
2026-01-04 12:06:51 +0800

    clean(frontend): 移除配置面板deadcode

--- applications/webui-frontend/src/pages/config-panel/components/index.ts
+++ applications/webui-frontend/src/pages/config-panel/components/index.ts
@@ -3,6 +3,5 @@
  */
 export * from "./inputs/index";
 export * from "./editors/index";
-export * from "./sections/index";
 export * from "./schema/index";
 export { default as ConfigSidebar } from "./ConfigSidebar";
